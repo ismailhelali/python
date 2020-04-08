5dde5d9055f75b1e122ce21f778c614402ca7d8b
ismailhelali <40539850+ismailhelali@users.noreply.github.com>
2020-04-08 22:27:45 +0200

    Update application.py

--- application.py
+++ application.py
@@ -3,3 +3,6 @@ app=Flask(__name__)
 def index():
 names=["Mohamed","Driss","Rachida","Fatima","Ismail",]
 return render_template("index.html",names=names)
+
+if __name__ == "__main__":
+  app.run(debug=True) 
